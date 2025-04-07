c2e2d908092a05609fe8840fc7f19710a1592a19
JohariGandawali <joharigandawali114@gmail.com>
2025-04-07 10:53:27 +0800

    Initial commit

---
+++ ddd.py
@@ -0,0 +1,37 @@
+class Samsung_A54_5G:
+    def make_call(self):
+        print("makatawag ka ug kusog ni siya kay 5G")
+
+    def make_gaming(self):
+        print("gaming phone ni siya nga cellphone ")
+
+    def fast_charging(self):
+        print("As in dali ni siya ma full kay super charging ni nga cellphone")
+
+    def storage(self):
+        print("dakog storage ni mga 512GB")
+
+    def Super(self):
+        print("hayag pa kaayu ni siya naka Super Amoled")
+
+    def Price(self):
+        print("mahal ni siya pag kuha naku nasa 25K")
+
+    def Smooth(self):
+        print("naka 120 refresh rate pa, so smooth gyud siya")
+
+    def Model(self):
+        print("model ani nga cellphone kay mga 2023-2024 goods gyapon")
+
+    def Super(self):
+        print("hayag pa kaayu ni siya naka Super Amoled")
+
+S1 = Samsung_A54_5G()
+S1.make_call()
+S1.make_gaming()
+S1.fast_charging()
+S1.storage()
+S1.Super()
+S1.Price()
+S1.Smooth()
+S1.Model()
